a53263d5227b8a1a33b1df4db92cd06f13edcb5f
nchernetsov <n.chernetsov86@gmail.com>
2019-12-07 01:17:41 +0300

    Добавлена таблица бонусов

--- rosbank-12-2019/src/main/resources/sql/create-db.sql
+++ rosbank-12-2019/src/main/resources/sql/create-db.sql
@@ -1,3 +1,4 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 create table transactions (
     id          uuid    PRIMARY KEY,
@@ -9,3 +10,17 @@ create table transactions (
     time        timestamptz
 );
 
+create table bonuses (
+    id              uuid    PRIMARY KEY  DEFAULT uuid_generate_v1(),
+    client_id       uuid,
+    amount          numeric(15,2),
+    create_time     timestamptz,
+    update_time     timestamptz,
+    strategy_id     uuid
+);
+
+create table bonuses_transactions (
+     bonus_uuid         uuid  REFERENCES bonuses (id) ON DELETE CASCADE,
+     transaction_uuid   uuid  REFERENCES transactions (id) ON DELETE CASCADE,
+     CONSTRAINT bonuses_transactions_pkey PRIMARY KEY (bonus_uuid, transaction_uuid)
+);
