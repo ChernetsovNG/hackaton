59454968f5e39a9ac4a48d7cfe020e5e40308a66
nchernetsov <n.chernetsov86@gmail.com>
2019-12-06 21:14:18 +0300

    Init commit

---
+++ rosbank-12-2019/src/test/java/ru/rosbank/hackathon/bonusSystem/BonusSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rosbank.hackathon.bonusSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonusSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
